77f62d1ffcc28c8494a9c5011f96b11a740df1bf
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-19 23:36:13 +0530

    added

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,21 +1,28 @@
 import TotalScore from "./TotalScore";
 import NumberSelector from "./NumberSelector";
 import  styled from "styled-components"
+import RoleDice from "./RoleDice";
 
 const GamePlay = () => {
   return (
-    <maiMainContainern>
+    <MainContainer>
       <div className="top-section">
       <TotalScore/>
       <NumberSelector/>
       </div>
-    </maiMainContainern>
+      <RoleDice></RoleDice >
+    </MainContainer>
   );
 };
 
 export default GamePlay;
 
 const MainContainer= styled.main`
-.top-section{
+padding-top:30px;
+
+.top-section{  
   display: flex;
+  justify-content: space-around;
+  align-items: end;
+
 }`;
---
+++ src/components/RoleDice.jsx
@@ -0,0 +1,15 @@
+
+import styled from "styled-components";
+const RoleDice = () => {
+  return (
+    <DiceContainer>
+      <div>
+        <img src="public/one.png" alt="diceone" />
+      </div>
+    </DiceContainer>
+  );
+};
+
+export default RoleDice;
+
+const DiceContainer = styled.div``;
