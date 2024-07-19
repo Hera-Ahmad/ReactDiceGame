835dca85f14d19ee0848873101bb1a5785224595
Hera Ahmad <ahmadhera0512@gmail.com>
2024-07-19 13:01:06 +0530

    start all

--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -15,7 +15,16 @@ export default StartGame;
 
 const Container = styled.div`
   max-width: 1180px;
+  display:flex;
+  margin: 0 auto;
 `;
 
-const Button= styled.button`
+const Button = styled.button`
+padding: 10px 18px;
+gap:10px;
+min-width:220px;
+background: black;
+color:white;
+border-radius:5px;
+border:none;
 `;
